6f2aeb96bafabdf6303f049b8d04c7497b441675
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-07-08 13:02:10 +0900

    dev wait time

--- src/main/java/com/msa/aws/msk/msk_consumer/MessageReceiver.java
+++ src/main/java/com/msa/aws/msk/msk_consumer/MessageReceiver.java
@@ -3,17 +3,43 @@ package com.msa.aws.msk.msk_consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class MessageReceiver {
 
     @KafkaListener(topics="Topic_from_java")
     public void receiveMessage(ConsumerRecord<String, String> record){
-        System.out.println("Received message:");
-        System.out.println("Key: " + record.key());
-        System.out.println("Value: " + record.value());
-        System.out.println("Partition: " + record.partition());
-        System.out.println("Offset: " + record.offset());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        System.out.println("PROCESSING START =======================================================" );
+
+        //処理開始時間の表示
+        LocalDateTime now_bf = LocalDateTime.now();
+        System.out.println("START TIME：" + formatter.format(now_bf)+" & MESSAGE key： "+record.key());
+        System.out.println("START TIME：" + formatter.format(now_bf)+" & MESSAGE Value： "+record.value());
+        System.out.println("START TIME：" + formatter.format(now_bf)+" & MESSAGE partition： "+record.partition());
+        System.out.println("START TIME：" + formatter.format(now_bf)+" & MESSAGE Offset： "+record.offset());
+
+        int waitTime = Integer.parseInt(record.value()) * 1000;
+        System.out.println("wait time: " + waitTime);
+        waitInMilliseconds(waitTime);
+
+        //処理完了時間の表示
+        LocalDateTime now_af = LocalDateTime.now();
+        System.out.println("END TIME： " + formatter.format(now_af) +" & MESSAGE ID： "+record.key());
+        System.out.println("PROCESSING END =======================================================" );
+
+    }
+    //数字を受け取って、その時間待機するためのメソッド
+    private void waitInMilliseconds(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
     }
 
 }
