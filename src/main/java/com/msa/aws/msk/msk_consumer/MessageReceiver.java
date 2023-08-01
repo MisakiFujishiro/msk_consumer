965735ce051eece674ec611f417e8cf22c07fd18
Misaki.Fujishiro <Misaki.Fujishiro@nttdata.com>
2023-08-01 10:53:18 +0900

    add log info all

--- src/main/java/com/msa/aws/msk/msk_consumer/MessageReceiver.java
+++ src/main/java/com/msa/aws/msk/msk_consumer/MessageReceiver.java
@@ -10,7 +10,7 @@ import java.time.format.DateTimeFormatter;
 @Component
 public class MessageReceiver {
 
-    @KafkaListener(topics="Topic_always")
+    @KafkaListener(topics="Topic_retention_part")
     public void receiveMessage(ConsumerRecord<String, String> record, Acknowledgment acknowledgment){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
